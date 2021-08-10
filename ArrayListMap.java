54e5d4138dbc56f26e63712bb58d981c39a15531
Fernanda Garcia Camargo <60704470+fegarciac@users.noreply.github.com>
2021-08-10 15:20:12 -0400

    Update ArrayListMap.java

--- ArrayListMap.java
+++ ArrayListMap.java
@@ -1,19 +1,6 @@
-//---------------------------------------------------------------------------
-// ArrayListMap.java          by Dale/Joyce/Weems                   Chapter 8
-//
-// Implements a map using an ArrayList.
-//
-// A map provides (K = key, V = value) pairs, mapping the key onto the value.
-// Keys are unique. Keys cannot be null.
-//
-// Methods throw IllegalArgumentException if passed a null key argument.
-//
-// Values can be null, so a null value returned by put, get, or remove does
-// not necessarily mean that an entry did not exist.
-//---------------------------------------------------------------------------
 
 
-import java.util.*;  // Iterator, ArrayList
+import java.util.*; 
 
 public class ArrayListMap<K, V>  implements MapInterface<K,V>
 {
@@ -30,10 +17,7 @@ public class ArrayListMap<K, V>  implements MapInterface<K,V>
   }
 
   public V put(K k, V v)
-  // If an entry in this map with key k already exists then the value
-  // associated with that entry is replaced by value v and the original
-  // value is returned; otherwise, adds the (k, v) pair to the map and
-  // returns null.
+
   {
     if (k == null)
       throw new IllegalArgumentException("Maps do not allow null keys.");
@@ -41,7 +25,7 @@ public class ArrayListMap<K, V>  implements MapInterface<K,V>
     MapEntry<K, V> entry = new MapEntry<K, V>(k, v);
 
     MapEntry<K,V> temp;
-    Iterator<MapEntry<K,V>> search = map.iterator(); // Arraylist iterator
+    Iterator<MapEntry<K,V>> search = map.iterator(); 
     while (search.hasNext())
     {
       temp = search.next();
@@ -49,90 +33,86 @@ public class ArrayListMap<K, V>  implements MapInterface<K,V>
       {
         search.remove();
         map.add(entry);
-        return temp.getValue(); // k found, exits method
+        return temp.getValue(); 
       }
     }
 
-    // No entry is associated with k.
+  
     map.add(entry);
     return null;
   }
 
   public V get(K k)
-  // If an entry in this map with a key k exists then the value associated
-  // with that entry is returned; otherwise null is returned.
+ 
   {
     if (k == null)
       throw new IllegalArgumentException("Maps do not allow null keys.");
 
-    for (MapEntry<K,V> temp: map)   // uses ArrayList iterator
+    for (MapEntry<K,V> temp: map)   
       if (temp.getKey().equals(k))
-        return temp.getValue();     // k found, exits method
+        return temp.getValue();     
 
-    // No entry is associated with k.
+
     return null;
   }
 
   public V remove(K k)
-  // If an entry in this map with key k exists then the entry is removed
-  // from the map and the value associated with that entry is returned;
-  // otherwise null is returned.
+
   {
     if (k == null)
       throw new IllegalArgumentException("Maps do not allow null keys.");
 
     MapEntry<K,V> temp;
-    Iterator<MapEntry<K,V>> search = map.iterator(); // Arraylist iterator
+    Iterator<MapEntry<K,V>> search = map.iterator(); 
     while (search.hasNext())
     {
       temp = search.next();
       if (temp.getKey().equals(k))
       {
         search.remove();
-        return temp.getValue();    // k found, exits method
+        return temp.getValue();    d
       }
     }
 
-    // No entry is associated with k.
+    
     return null;
   }
 
   public boolean contains(K k)
-  // Returns true if an entry in this map with key k exists;
-  // Returns false otherwise.
+
   {
     if (k == null)
       throw new IllegalArgumentException("Maps do not allow null keys.");
 
     for (MapEntry<K,V> temp: map)
       if (temp.getKey().equals(k))
-        return true;     // k found, exits method
+        return true;    
 
-    // No entry is associated with k.
+  
     return false;
   }
 
   public boolean isEmpty()
-  // Returns true if this map is empty; otherwise, returns false.
+  
   {
-    return (map.size() == 0);   // uses ArrayList size
+    return (map.size() == 0);   
   }
 
   public boolean isFull()
-  // Returns true if this map is full; otherwise, returns false.
+  
   {
-    return false;  // An ArrayListMap is never full
+    return false;  
   }
 
   public int size()
-  // Returns the number of entries in this map.
+
   {
-    return map.size();   // uses ArrayList size
+    return map.size();   
   }
 
   public Iterator<MapEntry<K,V>> iterator()
-  // Returns the Iterator provided by ArrayList.
+ 
   {
-    return map.iterator();  // returns ArrayList iterator
+    return map.iterator();  
   }
 }
